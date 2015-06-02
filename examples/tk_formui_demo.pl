3b0f8b6bf6a21a62471e769359ba3c103d15a0e7
Paul <alabamapaul@gmail.com>
2015-06-02 14:25:59 -0500

    Example simplified by use of Tk::FormUI 1.0
    Tk::FormUI now performs validation, so this greatly simplifies the
    example.

--- examples/tk_formui_demo.pl
+++ examples/tk_formui_demo.pl
@@ -12,13 +12,13 @@ use warnings;
 use File::Basename qw(dirname fileparse basename);
 use File::Spec;
 ## Add script directory
-use lib join '/', File::Spec->splitdir(dirname(__FILE__));
+use lib File::Spec->catdir(File::Spec->splitdir(dirname(__FILE__)));
 ## Add script directory/lib
-use lib join '/', File::Spec->splitdir(dirname(__FILE__)), 'lib';
+use lib File::Spec->catdir(File::Spec->splitdir(dirname(__FILE__)), qq{lib});
 ## Add script directory/../lib
-use lib join '/', File::Spec->splitdir(dirname(__FILE__)), '..', 'lib';
+use lib File::Spec->catdir(File::Spec->splitdir(dirname(__FILE__)), qq{..}, qq{lib});
 use Readonly;
-use Tk::FormUI;
+use Tk::FormUI 1.0;
 use Data::Dumper;
 
 ## List reference for choices
@@ -45,6 +45,16 @@ Readonly::Scalar my $SURVEY_FORM => {
       width => 40,
       label => qq{Name},
       key   => qq{name},
+      validation =>
+        sub
+        {
+          my $field = shift;
+          my $data = $field->value;
+          $data =~ s/^\s+//g;  ## Remove leading spaces
+          $data =~ s/\s+$//g;  ## Remove trailing spaces
+          return if ($data);
+          return(qq{The name field cannot be empty!});
+        },
     },
     {
       type  => $Tk::FormUI::RADIOBUTTON,
@@ -57,12 +67,28 @@ Readonly::Scalar my $SURVEY_FORM => {
         { label => qq{Ms.},   value => qq{Ms.},   },
         { label => qq{Mr.},   value => qq{Mr.},   },
       ],
+      validation =>
+        sub
+        {
+          my $field = shift;
+          my $data = $field->value;
+          return if (defined($data));
+          return(qq{You must select a Title!});
+        },
     },
     {
       type  => $Tk::FormUI::COMBOBOX,
       label => qq{Current Continent},
       key   => qq{continent_residence},
       choices => $CONTINENT_CHOICES,
+      validation =>
+        sub
+        {
+          my $field = shift;
+          my $data = $field->value;
+          return if (defined($data));
+          return(qq{You must select your current continent of residence!});
+        },
     },
     {
       type  => $Tk::FormUI::CHECKBOX,
@@ -75,111 +101,9 @@ Readonly::Scalar my $SURVEY_FORM => {
 };
 
 ##----------------------------------------------------------------------------
-##     @fn get_valid_form($template)
-##  @brief Display the given form template and repeat until valid data is
-##         gathered or the operator aborts
-##  @param $template - Used to initialize the form
-## @return UNDEF if operator cancels, or HASH reference containing the
-##         gathered data
-##   @note 
+## Main code
 ##----------------------------------------------------------------------------
-sub get_valid_form
-{
-  my $template = shift;
-  
-  ## Main window for possible MessageBox calls
-  my $mw = MainWindow->new;
-  $mw->withdraw();
-  
-  ## Create a new form object
-  my $form = Tk::FormUI->new;
-  
-  ## Initialize the form using the given template
-  $form->initialize($template);
-  
-  ## Now gather data from the user using the form
-  my $data;
-  while (1)
-  {
-    ## Display the form
-    $data = $form->show;
-    
-    ## For demo purposes, print out the data receied
-    print(
-      qq{The form returned the following:\n},
-      Data::Dumper->Dump([$data,], [qw( data)]),
-      qq{\n},
-    );
-    
-    ## See if we received data
-    if ($data)
-    {
-      ##---------------------------------
-      ## Validate the name field
-      ##---------------------------------
-      $data->{name} =~ s/^\s+//g;  ## Remove leading spaces
-      $data->{name} =~ s/\s+$//g;  ## Remove trailing spaces
-      ## Validate the name field
-      unless ($data->{name})
-      {
-        $form->error_by_key(qq{name}, qq{The name field cannot be empty!});
-      }
-      
-      ##---------------------------------
-      ## Validate the title field
-      ##---------------------------------
-      unless (defined($data->{title}))
-      {
-        $form->error_by_key(qq{title}, qq{You must select a Title!});
-      }
-      
-      ##---------------------------------
-      ## Validate the continent_residence field
-      ##---------------------------------
-      unless (defined($data->{continent_residence}))
-      {
-        $data->{continent_residence} = qq{};
-        $form->error_by_key(
-          qq{continent_residence},
-          qq{You must select your current continent of residence!},
-        );
-      }
-      
-      ## Return the data if there are no errors
-      return($data) if (!$form->has_errors);
-
-      ## If we get here then there was at least one error
-      
-      ## Set the form's field data
-      $form->set_field_data($data);
-      
-      ## Display a popup
-      $mw->messageBox(
-        -title    => qq{ERROR},
-        -message  => qq{There were errors in the provided data!},
-        -type     => qq{Ok},
-      );
-    }
-    else
-    {
-      ## Operator canceled the form, see if they want to abort
-      my $answer = $mw->messageBox(
-        -title    => qq{Tk::FormUI Demo},
-        -message  => qq{\nDo you want to abort the test?\n},
-        -type     => qq{YesNo},
-      );
-      
-      ## Return undef if operator wants to cancel the form
-      return if (uc($answer) eq qq{YES});
-    }
-    
-  }
-  return;
-}
-
-##----------------------------------------------------------------------------
-
-my $data = get_valid_form($SURVEY_FORM);
+my $data = Tk::FormUI->new()->initialize($SURVEY_FORM)->show;
 
 print(
   qq{The following data was returned:\n},
